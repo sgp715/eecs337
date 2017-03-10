5d2f60e42e847d0e0b313fb5a7ca43e9131b67f6
Mertzstache <ericm338@Erics-MacBook-Pro-2.local>
2017-03-10 13:21:53 -0600

    linking things together

--- app.py
+++ app.py
@@ -19,6 +19,26 @@ def home():
 def popular():
     return render_template("popular.html")
 
+@app.route('/business', methods=['GET'])
+def business():
+    return render_template("business.html")
+
+@app.route('/sports', methods=['GET'])
+def sports():
+    return render_template("sports.html")
+
+@app.route('/international', methods=['GET'])
+def international():
+    return render_template("international.html")
+
+@app.route('/political', methods=['GET'])
+def political():
+    return render_template("political.html")
+
+@app.route('/science', methods=['GET'])
+def science():
+    return render_template("science.html")
+
 @app.route('/something_new', methods=['GET'])
 def something_new():
     return render_template("something_new.html")
